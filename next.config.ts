bbcfaadb210af0bf86bc949fbc4af8070cdf4462
Dirisu Paul <62507389+osigbemhe2020@users.noreply.github.com>
2026-04-30 12:12:48 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_BACKEND_URL: 'https://site--hng14-backend--nlrjqkv9zhwn.code.run/'
+    NEXT_PUBLIC_BACKEND_URL: 'https://site--hng14-backend--nlrjqkv9zhwn.code.run'
   }
 };
 
